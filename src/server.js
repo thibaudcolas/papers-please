16d8f4887ef0ce5a0de316da04ca398a42db370c
Thibaud Colas <thibaudcolas@gmail.com>
2015-07-04 05:03:59 +1200

    Add basic client/server React + Express app

---
+++ src/server.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+});
+
+app.get('/hello', function(req, res) {
+    res.send(JSON.stringify({
+        test: true
+    }));
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running at localhost:' + app.get('port'));
+});
