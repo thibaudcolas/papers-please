fe76c57b4d606f57af6cacd6c46a1d7878f68eef
Thibaud Colas <thibaudcolas@gmail.com>
2015-07-04 16:05:52 +1200

    Add file read + send to analysis service

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 var path = require('path');
+var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
+var request = require('request');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -20,7 +22,21 @@ app.get('/app', function(req, res) {
 });
 
 app.post('/submit-paper', function(req, res){
-    res.send(JSON.stringify(req.files));
+    var filePath = req.files.paper.path;
+
+    fs.readFile(filePath, {encoding: 'utf-8'}, function(err, data) {
+        if (err) throw err;
+        request.post({
+            url:'http://expresso-app.org/analyze-text',
+            form: {
+                html: data
+            }
+        }, function(err, response, body){
+            if (!err && response.statusCode == 200) {
+              res.send(body);
+            }
+        });
+    });
 });
 
 app.get('/hello', function(req, res) {
